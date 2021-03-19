79ca42b605517a81b1d4c8a36b3ba5b422ea395a
ayonelahi <mirelahi33@gmail.com>
2021-03-18 20:39:45 -0700

    Documentation and comment added in code

--- src/controller/BattleshipController.java
+++ src/controller/BattleshipController.java
@@ -11,32 +11,55 @@ import model.BattleShip;
 
 import java.util.*;
 
+/**
+ * This is the main controller for Battleship Game.
+ * All the data is accessed from user or model and logics are generated here
+ */
 public class BattleshipController {
 
+    // variable for labels - one for message displaying in top and other is for chances remaining bottom
     public Label lblMessage;
     public Label lblChances;
+
+    // counter for chances remaining and ship sunk
     int chancesRemaining=20;
     int shipSunk=0;
-    ArrayList<String> testShip = new ArrayList<String>();
+
+    //variable for handling first time execution only situations
     boolean firstTime=true,secondTime=true;
     boolean firstTimeGenerate=true;
-    String[] shipLocation = new String[3];
-    String[] shipLocation2 = new String[3];
-    ArrayList<String[]> myList = new ArrayList<String[]>();
-    BattleShip battleShip = new BattleShip();
-    public void onBtnClicked(MouseEvent mouseEvent) {
-        Button btnSelected=(Button) mouseEvent.getSource();
-        btnSelected.setDisable(true);
-        btnSelected.getStyleClass().add("miss");
 
+    //variable for entering all the  hit location from the user in the board
+    ArrayList<String> testShip = new ArrayList<String>();
+
+    // creating an object for battleship model
+    BattleShip battleShip = new BattleShip();
+
+
+    /**
+     * all the cell selected and hit or miss , win of loss will be calculated here
+     * @param mouseEvent this is the info of selected button by user
+     */
+    public void onBtnClicked(MouseEvent mouseEvent) {
+        //from the mouseevent we got the specific selected button or cell info
+        Button btnSelected=(Button) mouseEvent.getSource();
+
+        // after clicking any cell in the board. that cell will be disable. So the first time input in a cell is accepted.
+        btnSelected.setDisable(true);
+
+        //we initialized any selected cell as miss first and if its hit it will be showed as hit
+        btnSelected.getStyleClass().add("miss");
         lblMessage.setText("Miss");
+        // if miss chances remaining counter will be decreased by 1
         chancesRemaining--;
 
+        //the ships will be generated on the board and its first time only
         if(firstTimeGenerate) {
             firstTimeGenerate=false;
             battleShip.generateShip();
         }
 
+        //if the generated ship and selected cell is matched it is a hit
         for(String[] singleShip : battleShip.getMyList()) {
             for (int i = 0; i < singleShip.length; i++) {
                 if(btnSelected.getId().equals(singleShip[i])){
@@ -48,6 +71,7 @@ public class BattleshipController {
             }
             List singleShipList = Arrays.asList(singleShip);
             int singleHitCount = 0;
+            //here we hanle if whole ship is sunk or not
             for (int i = 0; i < testShip.size(); i++) {
                 if (singleShipList.contains(testShip.get(i))) {
 
@@ -70,6 +94,8 @@ public class BattleshipController {
 
 
         }
+
+        //if chances remaining become 0. So the game is over and lost. and it will show a warning panel then after clicking that the board will exit after 1 second.
         if(chancesRemaining==0){
             chancesRemaining=0;
         lblMessage.setText("You Lost!!  Game over");
@@ -81,9 +107,12 @@ public class BattleshipController {
             }, 1000);
         }
 
+        //show the chances remaining in bottom
         if(chancesRemaining>=0) {
             lblChances.setText(chancesRemaining + " Chances Remaining");
         }
+
+        //if both 2 generated ships are sunk and before finishing remaining chances then gave over and won.
         if(shipSunk==2){
             lblMessage.setText("You Win! You sank my Battleship");
             Alert alert = new Alert(Alert.AlertType.WARNING, "You Win! You sank my Battleship!", ButtonType.OK, ButtonType.CANCEL);
--- src/model/BattleShip.java
+++ src/model/BattleShip.java
@@ -4,36 +4,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This is the Battleship model where generate ship and those data will be handled
+ */
 public class BattleShip {
 
-    String[] shipLocation = new String[3], shipLocation2 = new String[3], shipLocation3 = new String[3];
+    /**
+     * we create 2 array for 2 ships.
+     */
 
+    String[] shipLocation = new String[3], shipLocation2 = new String[3];
 
+//all the ships  will be saved in a arraylist called myList
     ArrayList<String[]> myList = new ArrayList<String[]>();
 
-
+    /**
+     *
+     * @return  all the ships in an arraylist
+     */
     public ArrayList<String[]> getMyList() {
         return myList;
     }
 
+    /**
+     *
+     * @param myList all the ships in an arraylist
+     */
     public void setMyList(ArrayList<String[]> myList) {
         this.myList = myList;
     }
 
-    public String[] getShipLocation() {
-        return shipLocation;
-    }
 
-    public void setShipLocation(String[] shipLocation) {
-        this.shipLocation = shipLocation;
-    }
-
+    /**
+     * This function will be called once the begining of the program
+     * create 2 random numbers from 1-108
+     * with those random numbers 2 ships will be created
+     * one horizontal and one vertical ship
+     * finally those ships will be added in myList
+     *
+     */
     public void generateShip() {
+        //2 random numbers are creatd
         int randomShip1 = (int) (Math.random() * (108 - 1) + 1);
         int randomShip2 = (int) (Math.random() * (108 - 1) + 1);
+
+        //make sure both 2 random numbers are not same and those are not collided in horizontal and vertical
+        //if happens we use a reccursive method to handle that
         if (randomShip1 == randomShip2 || randomShip1 + 1 == randomShip2 || randomShip1 + 2 == randomShip2) {
             generateShip();
         }
+
+        //here we create 1 ship as horizontal and 1 as vertical then added to an arraylist
+        //System.out.println("")  is used to see the horizontal and vertical created ship location for tested purpose
         for (int i = 0; i < 3; i++) {
             shipLocation[i] = String.valueOf(randomShip1);
             shipLocation2[i] = String.valueOf(randomShip2);
